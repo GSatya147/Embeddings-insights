bf34f745f5f910b01b2c2981c9c36f8923944a66
GSatya147 <gvvsn147@gmail.com>
2026-05-23 13:00:00 +0100

    code: add embedding client, chroma initialisation

--- vector_dbs/vector_dbs.py
+++ vector_dbs/vector_dbs.py
@@ -1,1 +1,17 @@
-import litellm
+from google import genai
+from google.genai import types
+from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding_client = genai.Client()
+
+result = embedding_client.models.embed_content(
+                model="gemini-embedding-001",
+                contents=["Hi, i'm satya"],
+                config=types.EmbedContentConfig(output_dimensionality=3072)
+            )
+
+print(result.embeddings[0].values)
+
